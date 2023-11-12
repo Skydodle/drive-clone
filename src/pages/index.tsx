ed9e93903197df79ec288de8471c08e9f88bd0cf
Johnny Wu <skydodle@gmail.com>
2023-11-11 17:35:22 -0800

    Added sign out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+  // console.log("session:", session);
   return (
     <>
       <Head>
@@ -14,13 +15,21 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Welcome {session?.user.name}
           </h1>
-          <Button
-            onClick={() => signIn()}
-            btnClass="btn-primary"
-            title="Sign Up!"
-          />
+          {session ? (
+            <Button
+              onClick={() => signOut()}
+              btnClass="btn-secondary"
+              title="Sign Out"
+            />
+          ) : (
+            <Button
+              onClick={() => signIn()}
+              btnClass="btn-primary"
+              title="Sign In"
+            />
+          )}
         </div>
       </main>
     </>
